3c4149752f4d7aa1178ae811c188c0e03aaea9df
samuelssan28 <samuelssan28@gmail.com>
2021-03-03 19:33:06 -0300

    add create_vereadores.py

---
+++ orm_python_sqlite/create_vereadores.py
@@ -0,0 +1,82 @@
+from index import Vereadores
+
+# Inserimos um Vereador 
+vereador_1 = Vereadores.create(nome='ALAN BRANDÃO',perfil="@alanbrandaothe")
+
+# Inserimos um Vereador 
+vereador_2 = Vereadores.create(nome='ALUÍSIO SAMPAIO',perfil="@aluisiosampaio")
+
+# Inserimos um Vereador 
+vereador_3 = Vereadores.create(nome='BRUNO VILARINHO',perfil="@brunovilarinho14")
+
+# Inserimos um Vereador 
+vereador_4 = Vereadores.create(nome='CAPITÃO ROBERVAL QUEIROZ',perfil="@vereadorcaprobervalqueiroz")
+
+# Inserimos um Vereador 
+vereador_5 = Vereadores.create(nome='DEOLINDO MOURA',perfil="@deolindo.moura")
+
+# Inserimos um Vereador 
+vereador_6 = Vereadores.create(nome='DR. LEONARDO EULÁLIO',perfil="@vereadordudu")
+
+# Inserimos um Vereador 
+vereador_7 = Vereadores.create(nome='EDILBERTO BORGES',perfil="@vereadordudu")
+
+# Inserimos um Vereador 
+vereador_8 = Vereadores.create(nome='EDSON MELO',perfil="@edsonmsmelo")
+
+# Inserimos um Vereador 
+vereador_9 = Vereadores.create(nome='ELZUILA CALISTO',perfil="@elzuilac")
+
+# Inserimos um Vereador 
+vereador_10 = Vereadores.create(nome='ENZO SAMUEL',perfil="@vereadorenzosamuel")
+
+# Inserimos um Vereador 
+vereador_11 = Vereadores.create(nome='EVANDRO HIDD',perfil="@vereadorafernandagomes")
+
+# Inserimos um Vereador 
+vereador_12 = Vereadores.create(nome='GUSTAVO DE CARVALHO',perfil="@vereadorgustavodecarvalho")
+
+# Inserimos um Vereador 
+vereador_13 = Vereadores.create(nome='ISMAEL SILVA',perfil="@ismaelsilvathe")
+
+# Inserimos um Vereador 
+vereador_14 = Vereadores.create(nome='JEOVÁ ALENCAR',perfil="@@jeovaalencar")
+
+# Inserimos um Vereador 
+vereador_15 = Vereadores.create(nome='JOAQUIM CALDAS',perfil="@vereadorjoaquimcaldas")
+
+# Inserimos um Vereador 
+vereador_16 = Vereadores.create(nome='LEVINO DE JESUS',perfil="@levinodejesus")
+
+# Inserimos um Vereador 
+vereador_17 = Vereadores.create(nome='LUÍS ANDRÉ',perfil="@luisandrevereador")
+
+# Inserimos um Vereador 
+vereador_18 = Vereadores.create(nome='LUIZ LOBÃO',perfil="@luizlobao_")
+
+# Inserimos um Vereador 
+vereador_19 = Vereadores.create(nome='MARKIM COSTA',perfil="@markimcosta")
+
+# Inserimos um Vereador 
+vereador_20 = Vereadores.create(nome='NETO DO ANGELIM',perfil="@netodoangelimvereador")
+
+# Inserimos um Vereador 
+vereador_21 = Vereadores.create(nome='PAULO LOPES',perfil="@paulolopesteresina")
+
+# Inserimos um Vereador 
+vereador_22 = Vereadores.create(nome='POLLYANNA ROCHA',perfil="@sem")
+
+# Inserimos um Vereador 
+vereador_23 = Vereadores.create(nome='THANANDRA SARAPATINHAS',perfil="@thanandra.sarapatinhase")
+
+# Inserimos um Vereador 
+vereador_24 = Vereadores.create(nome='RENATO BERGER',perfil="@renato.berger")
+
+# Inserimos um Vereador 
+vereador_25 = Vereadores.create(nome='TERESINHA MEDEIROS',perfil="@")
+
+# Inserimos um Vereador 
+vereador_26 = Vereadores.create(nome='VALDEMIR VIRGINIO',perfil="@valdemirvirgino")
+
+# Inserimos um Vereador 
+vereador_27 = Vereadores.create(nome='VENÂNCIO',perfil="@vereador.venancio")
